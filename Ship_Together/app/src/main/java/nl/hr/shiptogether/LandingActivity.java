7001f56245a1545776ea1d68074e1a607a0112f0
RoyvandenHeuvel <0878963@hr.nl>
2016-04-25 17:17:19 +0200

    dummy login

--- Ship_Together/app/src/main/java/nl/hr/shiptogether/LandingActivity.java
+++ Ship_Together/app/src/main/java/nl/hr/shiptogether/LandingActivity.java
@@ -1,16 +1,19 @@
 package nl.hr.shiptogether;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LandingActivity extends AppCompatActivity {
+public class LandingActivity extends AppCompatActivity implements View.OnClickListener {
 
+
+    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +21,9 @@ public class LandingActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        loginButton = (Button) findViewById(R.id.button);
+
+        loginButton.setOnClickListener(LandingActivity.this);
     }
 
     @Override
@@ -48,5 +46,21 @@ public class LandingActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        EditText username = (EditText) findViewById(R.id.editText);
+        EditText password = (EditText) findViewById(R.id.editText2);
+
+        if(String.valueOf(username.getText()).equals("username") && String.valueOf(password.getText()).equals("password")){
+            System.out.println("Gefeliciteerd, u bent ingelogd!");
+            Intent intent = new Intent(this, MainActivity.class);
+
+            startActivity(intent);
+        } else {
+            System.out.println("Probeer het nog maar eens een keer.");
+        }
     }
 }
