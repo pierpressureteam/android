d8de1e505a027cd80bd3e0aaffba178e937d9302
GovertJaap <0892489@hr.nl>
2016-05-19 13:37:27 +0200

    Added library's for graphs called MPAndroidCharts, and the socketClient for connecting to the server.

--- Ship_Together/app/src/main/java/nl/hr/shiptogether/DataActivity.java
+++ Ship_Together/app/src/main/java/nl/hr/shiptogether/DataActivity.java
@@ -3,6 +3,11 @@ package nl.hr.shiptogether;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.IOException;
+
+import socketclient.SocketClient;
+
+
 public class DataActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +15,12 @@ public class DataActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
     }
+    /*
+    public void connectToServer() throws IOException, ClassNotFoundException {
+        Object object = 1;
+        SocketClient socketClient = new SocketClient();
+        Object socketObject = socketClient.communicateWithSocket(object);
+
+    }
+    */
 }
