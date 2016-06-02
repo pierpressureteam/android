b9898b3dce26cf77d7fd621f6f34961929da6b2e
GovertJaap <0892489@hr.nl>
2016-06-02 15:26:13 +0200

    Login in now uses socketconnection and is now fully operational. Graphs are under construction

--- Ship_Together/app/src/main/java/nl/hr/shiptogether/GraphActivity.java
+++ Ship_Together/app/src/main/java/nl/hr/shiptogether/GraphActivity.java
@@ -1,7 +1,17 @@
 package nl.hr.shiptogether;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+
 
 public class GraphActivity extends AppCompatActivity {
 
@@ -9,5 +19,45 @@ public class GraphActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+
+        CreateLineChart();
+
     }
+    public LineChart CreateLineChart() {
+        LineChart lineChart = (LineChart) findViewById(R.id.chart);
+
+        LineDataSet newData = CreateLinechartDataSet();
+        ArrayList<String> labels = CreateLabelsForLineChart();
+
+        LineData data = new LineData(labels, newData);
+        lineChart.setData(data); // set the data and list of lables into chart
+        return lineChart;
+    }
+
+    public LineDataSet CreateLinechartDataSet() {
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(8f, 1));
+        entries.add(new Entry(6f, 2));
+        entries.add(new Entry(12f, 3));
+        entries.add(new Entry(18f, 4));
+        entries.add(new Entry(9f, 5));
+
+        LineDataSet dataset = new LineDataSet(entries, "# of Calls");
+        return dataset;
+    }
+
+    public ArrayList<String> CreateLabelsForLineChart() {
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+        return labels;
+    }
+
+
+
 }
--- Ship_Together/app/src/main/java/nl/hr/shiptogether/LandingActivity.java
+++ Ship_Together/app/src/main/java/nl/hr/shiptogether/LandingActivity.java
@@ -1,6 +1,7 @@
 package nl.hr.shiptogether;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -18,7 +20,46 @@ import objectslibrary.SocketObjectWrapper;
 
 public class LandingActivity extends AppCompatActivity implements View.OnClickListener {
 
+    class NetworkHandler extends AsyncTask<SocketObjectWrapper, Void, Boolean> {
+        private Exception exception;
+        SocketClient sc = new SocketClient();
+
+        @Override
+        protected Boolean doInBackground(SocketObjectWrapper... params) {
+            Boolean success = false;
+            SocketObjectWrapper sow = params[0];
+
+            try {
+                success = (boolean) sc.communicateWithSocket(sow);
+                return success;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("nope");
+                return null;
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("Le nope");
+                return null;
+            }
+        }
+
+        protected void onPostExecute(Boolean success) {
+
+
+            if (success) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(intent);
+            } else {
+                Toast toast = Toast.makeText(getApplicationContext(), "Username or password is incorrect", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+        }
+    }
+
     private Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,20 +76,13 @@ public class LandingActivity extends AppCompatActivity implements View.OnClickLi
                 openAcountCreation();
             }
         });
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                SocketHandler socketHandler = new SocketHandler();
-                socketHandler.socketConnection();
-            }
-        }).start();
+
     }
 
-    public void openAcountCreation(){
+    public void openAcountCreation() {
         Intent intent = new Intent(this, CreateAccount.class);
         startActivity(intent);
     }
-
 
 
     @Override
@@ -82,29 +116,19 @@ public class LandingActivity extends AppCompatActivity implements View.OnClickLi
 
         SocketClient sc = new SocketClient();
 
-        String sUsername = String.valueOf(username);
-        String sPassword = String.valueOf(password);
+        String sUsername = username.getText().toString();
+        String sPassword = password.getText().toString();
+
+        System.out.println(sUsername);
+        System.out.println(sPassword);
 
         User user = new User(sUsername, sPassword, "email currently not being validated!");
         SocketObjectWrapper sow = new SocketObjectWrapper(user, 1);
 
-        boolean success = false;
+        new NetworkHandler().execute(sow);
 
-        try {
-            success = (boolean) sc.communicateWithSocket(sow);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        if(success){
-            System.out.println("Gefeliciteerd, u bent ingelogd!");
-
-            Intent intent = new Intent(this, MenuActivity.class);
-            startActivity(intent);
-        } else {
-            System.out.println("Probeer het nog maar eens een keer.");
-        }
     }
+
 }
+
+
