3be330f5956f03548e68292f6afdd803ce3c92f9
AM Vrijenhoek <arjenhoek1994@hotmail.com>
2016-06-03 10:21:54 +0200

    -

--- Ship_Together/app/src/main/java/nl/hr/shiptogether/CreateAccount.java
+++ Ship_Together/app/src/main/java/nl/hr/shiptogether/CreateAccount.java
@@ -16,26 +16,28 @@ public class CreateAccount extends AppCompatActivity {
 
 
         final Button button = (Button) findViewById(R.id.btnCreateAcc);
-        button.setOnClickListener(new View.OnClickListener() {
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
 
-            EditText name = (EditText) findViewById(R.id.txtUsername);
-            EditText pass = (EditText) findViewById(R.id.txtPassword);
-            EditText repeatPass = (EditText) findViewById(R.id.txtRepeatPassword);
+                EditText name = (EditText) findViewById(R.id.txtUsername);
+                EditText pass = (EditText) findViewById(R.id.txtPassword);
+                EditText repeatPass = (EditText) findViewById(R.id.txtRepeatPassword);
 
-            //todo check if username is availible
+                //todo check if username is availible
 
-            //check if password is valid
-            public void onClick(View v) {
-                if(pass.getText().toString().length() <6){
-                    notifyUser("Password needs to be at least 6 characters");
+                //check if password is valid
+                public void onClick(View v) {
+                    if(pass.getText().toString().length() <6){
+                        notifyUser("Password needs to be at least 6 characters");
+                    }
+                    else if(!pass.getText().toString().equals(repeatPass.getText().toString())){
+                        notifyUser("The repeat password is not the same as the password.");
+                    }
+                    //todo save to database
+
                 }
-                else if(!pass.getText().toString().equals(repeatPass.getText().toString())){
-                    notifyUser("The repeat password is not the same as the password.");
-                }
-                //todo save to database
-
-            }
-        });
+            });
+        }
     }
 
     private void notifyUser(String text){
