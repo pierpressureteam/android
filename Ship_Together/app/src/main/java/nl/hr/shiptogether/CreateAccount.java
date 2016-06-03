c00e5e5b1010a957d4dde8601737fd4f88541e39
RoyvandenHeuvel <0878963@hr.nl>
2016-06-03 10:13:17 +0200

    account creation

--- Ship_Together/app/src/main/java/nl/hr/shiptogether/CreateAccount.java
+++ Ship_Together/app/src/main/java/nl/hr/shiptogether/CreateAccount.java
@@ -1,11 +1,19 @@
 package nl.hr.shiptogether;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.IOException;
+
+import objectslibrary.SocketObjectWrapper;
+import objectslibrary.User;
+import socketclient.SocketClient;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -31,8 +39,22 @@ public class CreateAccount extends AppCompatActivity {
                 }
                 else if(!pass.getText().toString().equals(repeatPass.getText().toString())){
                     notifyUser("The repeat password is not the same as the password.");
+                } else {
+                    EditText username = (EditText) findViewById(R.id.txtUsername);
+                    EditText password = (EditText) findViewById(R.id.txtPassword);
+
+                    String sUsername = username.getText().toString();
+                    String sPassword = password.getText().toString();
+
+                    System.out.println(sUsername);
+                    System.out.println(sPassword);
+
+                    User user = new User(sUsername, sPassword, "email currently not being entered!");
+                    SocketObjectWrapper sow = new SocketObjectWrapper(user, 2);
+
+                    new NetworkHandler().execute(sow);
                 }
-                //todo save to database
+
 
             }
         });
@@ -40,5 +62,43 @@ public class CreateAccount extends AppCompatActivity {
 
     private void notifyUser(String text){
         Toast.makeText(getApplicationContext(),text,  Toast.LENGTH_LONG).show();
+    }
+
+    class NetworkHandler extends AsyncTask<SocketObjectWrapper, Void, Boolean> {
+        private Exception exception;
+        SocketClient sc = new SocketClient();
+
+        @Override
+        protected Boolean doInBackground(SocketObjectWrapper... params) {
+            Boolean success = false;
+            SocketObjectWrapper sow = params[0];
+
+            try {
+                success = (boolean) sc.communicateWithSocket(sow);
+                return success;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("nope");
+                return null;
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                System.out.println("Le nope");
+                return null;
+            }
+        }
+
+        protected void onPostExecute(Boolean success) {
+
+
+            if (success) {
+                Intent intent = new Intent(getApplicationContext(), LandingActivity.class);
+                startActivity(intent);
+            } else {
+                Toast toast = Toast.makeText(getApplicationContext(), "Username and/or e-mail are already taken.", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+
+        }
     }
 }
