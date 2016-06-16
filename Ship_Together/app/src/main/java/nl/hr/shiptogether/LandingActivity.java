e61cd5c2179d8d3748a76db213d286ba2102ad32
GovertJaap <0892489@hr.nl>
2016-06-16 14:37:43 +0200

    Options menu is now deleted, on menu, graph button is now a normal button

--- Ship_Together/app/src/main/java/nl/hr/shiptogether/LandingActivity.java
+++ Ship_Together/app/src/main/java/nl/hr/shiptogether/LandingActivity.java
@@ -113,29 +113,7 @@ public class LandingActivity extends AppCompatActivity implements View.OnClickLi
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_landing, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, managementActivity.class);
-
-            startActivity(intent);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public void onClick(View v) {
